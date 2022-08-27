1177ef4bb2a38d724d98af913a1f3b2cb84845d1
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-26 22:17:26 -0400

    upload image- postman

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
+  // console.log(req); //necesitamos convertir la info de la imagen a un formato que se pueda leer, esto es con el paquete express-fileupload (en postman se usa form-data para asociar una imagen a la req)
   res.send('uploadProductImage');
 };
 
